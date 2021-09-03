4be659b58b4235247093b51c86a1a90beaddfa03
Marlena Tsang <Marlena345@gmail.com>
2021-09-03 15:28:53 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const main = document.querySelector('Main');
+main.remove();
+
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute ("id","victory");
+
+newHeader.innerHTML = "<h1>Marlena is the champion</h1>"
+
+document.body.append(victory);
